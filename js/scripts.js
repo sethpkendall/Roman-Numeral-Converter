538ba793027fea448391baac9211dc232b5c9ed5
Katrina Hirsch and Seth Kendall <katrina@>
2016-08-18 11:35:53 -0700

    complete business logic to satisfy all project specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,113 @@
 //Business Logic
 var validNumber = function(x) {
   var num2 = parseInt(x);
-
   if ((num2<1)||(num2>3999)){
     alert("Error: Invalid Number");
   } else {
-    console.log("something");
   }
+}
+
+var ones = function(num) {
+  var onesRes = "";
+  if (num==="1") {
+    onesRes +="I";
+  } else if (num==="2"){
+    onesRes +="II";
+  } else if (num==="3"){
+    onesRes +="III";
+  } else if (num==="4"){
+    onesRes +="IV";
+  } else if (num==="5"){
+    onesRes +="V";
+  } else if (num==="6"){
+    onesRes +="VI";
+  } else if (num==="7"){
+    onesRes +="VII";
+  } else if (num==="8"){
+    onesRes +="VIII";
+  } else if (num==="9"){
+    onesRes +="IX";
+  }
+  return onesRes;
+}
+
+var tens = function(num){
+  var tensRes = "";
+  if (num==="1") {
+    tensRes +="X";
+  } else if (num==="2"){
+    tensRes +="XX";
+  } else if (num==="3"){
+    tensRes +="XXX";
+  } else if (num==="4"){
+    tensRes +="XL";
+  } else if (num==="5"){
+    tensRes +="L";
+  } else if (num==="6"){
+    tensRes +="LX";
+  } else if (num==="7"){
+    tensRes +="LXX";
+  } else if (num==="8"){
+    tensRes +="LXXX";
+  } else if (num==="9"){
+    tensRes +="XC";
+  }
+  return tensRes;
+}
+
+var hundreds = function(num){
+  var hundredsRes = "";
+  if (num==="1") {
+    hundredsRes +="C";
+  } else if (num==="2"){
+    hundredsRes +="CC";
+  } else if (num==="3"){
+    hundredsRes +="CCC";
+  } else if (num==="4"){
+    hundredsRes +="CD";
+  } else if (num==="5"){
+    hundredsRes +="D";
+  } else if (num==="6"){
+    hundredsRes +="DC";
+  } else if (num==="7"){
+    hundredsRes +="DCC";
+  } else if (num==="8"){
+    hundredsRes +="DCCC";
+  } else if (num==="9"){
+    hundredsRes +="CM";
+  }
+  return hundredsRes;
+}
+
+var thousands = function(num){
+  var thousandsRes = "";
+  if (num==="1") {
+    thousandsRes +="M";
+  } else if (num==="2"){
+    thousandsRes +="MM";
+  } else if (num==="3"){
+    thousandsRes +="MMM";
+  }
+  return thousandsRes;
 }
 
 var conversion = function(number) {
   var numLength = number.length;
   var result = "";
   if (numLength===1) {
-    if (number==="1") {
-      result +="I";
-    } else if (number==="2"){
-      result +="II";
-    } else if (number==="3"){
-      result +="III";
-    } else if (number==="4"){
-      result +="IV";
-    } else if (number==="5"){
-      result +="V";
-    } else if (number==="6"){
-      result +="VI";
-    } else if (number==="7"){
-      result +="VII";
-    } else if (number==="8"){
-      result +="VIII";
-    } else if (number==="9"){
-      result +="IX";
-  // } else if (numLength===2) {
-  //
-  // } else if (numLength===3) {
-  //
-  // } else if (numLength===4) {
-
-    } else {
-    }
+    result += ones(number);
+  } else if (numLength===2) {
+    result += tens(number.slice(-2,-1));
+    result += ones(number.slice(-1));
+  } else if (numLength===3) {
+    result += hundreds(number.slice(-3,-2));
+    result += tens(number.slice(-2,-1));
+    result += ones(number.slice(-1));
+  } else if (numLength===4) {
+    result += thousands(number.slice(-4,-3));
+    result += hundreds(number.slice(-3,-2));
+    result += tens(number.slice(-2,-1));
+    result += ones(number.slice(-1));
   }
   return(result);
 }
@@ -52,7 +121,6 @@ $(document).ready(function(){
 
 //UI Logic
 $(document).ready(function(){
-
   $("form#input").submit(function(event){
     event.preventDefault();
     number = $("input#number").val() ;
